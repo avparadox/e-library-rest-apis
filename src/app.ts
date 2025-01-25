ec006e235bc9af86bf8e9f0ddec0e614e0eca4af
avparadox <avyas8927@gmail.com>
2025-01-25 21:25:30 +0530

    Added Error Handling Configuration

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+// import createHttpError from "http-errors";
+
 const app = express();
 
 // Routes
+// Http methods: GET, POST, PUT, PATCH, DELETE
 app.get("/", (req, res, next) => {
-  res.json({ message: "Hello Sir!" });
+  // const error = createHttpError(400, "something went wrong");
+  // throw error;
+  res.json({ message: "Welcome to elib apis" });
   next();
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
